137c3bedba469a566727ffc9089410751e3360b8
Fernando Ferreira de Lima Filho <fferreiralf@ufrn.edu.br>
2022-02-02 12:55:30 -0300

    criar funcionalidade de cadastrar e logar usuario

---
+++ app/src/main/java/br/com/ufrn/imd/dispositivos/todolist/TarefaActivity.java
@@ -0,0 +1,161 @@
+package br.com.ufrn.imd.dispositivos.todolist;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.ufrn.imd.dispositivos.todolist.fragments.EditItemFragment;
+import br.com.ufrn.imd.dispositivos.todolist.fragments.TodoItemDialog;
+import br.com.ufrn.imd.dispositivos.todolist.model.TodoItem;
+
+public class TarefaActivity extends AppCompatActivity
+        implements RecyclerViewAdapter.ItemClickListener, TodoItemDialog.OnSaveTodoItem, EditItemFragment.OnUpdateItem, EditItemFragment.OnDeleteItem {
+
+    FragmentManager fragmentManager;
+    RecyclerViewAdapter adapter;
+    SearchView simpleSearchView;
+    RecyclerView rvTodoList;
+    List<TodoItem> todoItemList;
+    List<TodoItem> todoItemListCopy;
+
+    private FloatingActionButton facbnewItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarefa);
+
+
+        fragmentManager = getSupportFragmentManager();
+
+        todoItemList = new ArrayList<>();
+        todoItemListCopy = new ArrayList<>();
+
+        simpleSearchView = findViewById(R.id.simpleSearchView);
+
+        adapter = new RecyclerViewAdapter(this, todoItemList, todoItemListCopy);
+        adapter.setClickListener(this);
+
+        rvTodoList =  findViewById(R.id.rvTodoList);
+        rvTodoList.setLayoutManager(new LinearLayoutManager(this));
+        rvTodoList.setAdapter(adapter);
+        simpleSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                adapter.filter(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.filter(newText);
+                return true;
+            }
+        });
+
+        facbnewItem = findViewById(R.id.facbnewItem);
+        facbnewItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TodoItemDialog todoItemDialog = TodoItemDialog.newInstance();
+                todoItemDialog.show(fragmentManager, TodoItemDialog.DIALOG_TAG);
+            }
+        });
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        TodoItem itemSelected = (TodoItem) todoItemList.get(position);
+
+        EditItemFragment editItemFragment = EditItemFragment.newInstance(itemSelected);
+        editItemFragment.show(fragmentManager, TodoItemDialog.DIALOG_TAG);
+    }
+
+    @Override
+    public void saveTodoItem(TodoItem todoItem) {
+        // set TodoItem id
+        Integer id;
+        if (todoItemList.size() == 0) {
+            id = 1;
+        }
+        else {
+            id = todoItemList.get(todoItemList.size() - 1).getId() + 1;
+        }
+
+        todoItem.setId(id);
+
+        todoItemList.add(todoItem);
+        todoItemListCopy.add(todoItem);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void updateItem(TodoItem todoItem) {
+        int id = todoItem.getId() - 1;
+        Log.d("id", "id: " + id);
+        todoItemList.set(id, todoItem);
+        todoItemListCopy.set(id, todoItem);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void deleteItem(TodoItem todoItem)
+    {
+        int id = todoItem.getId();
+        todoItemList.remove(id-1);
+        todoItemListCopy.remove(id-1);
+        updateIndex(id);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void updateIndex(int initialId){
+        for (int i=initialId+1; i<=todoItemList.size()+1; i++) {
+            TodoItem item = todoItemList.get(i-2);
+            item.setId(i-1);
+            todoItemList.set(i-2, item);
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    public void onComposeAction(MenuItem mi) {
+        new AlertDialog.Builder(this)
+                .setTitle("Membros da equipe")
+                .setMessage(" Maria Eduarda \n Fernando Ferreira \n André Herman \n Italo Silva")
+
+                .setPositiveButton(R.string.button_visitar_site, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        goToUrl("https://github.com/mariaeloi/dispositivos-moveis-projeto-1");
+                    }
+                })
+                .setNegativeButton(R.string.button_sair, null)
+                .show();
+    }
+}
---
+++ app/src/main/java/br/com/ufrn/imd/dispositivos/todolist/utils/DBHelper.java
@@ -0,0 +1,58 @@
+package br.com.ufrn.imd.dispositivos.todolist.utils;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper  extends SQLiteOpenHelper {
+
+    public static int VERSION = 1;
+    public static String NOME_BD = "todo_list_DB";
+    public static String TABELA_TODO = "todo_item";
+    public static String TABELA_USUARIO = "usuario";
+
+    public DBHelper(@Nullable Context context){
+        super(context,NOME_BD,null,VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String criarTabelaTodo = "CREATE TABLE IF NOT EXISTS " + TABELA_TODO
+                + "(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
+                + "descricao VARCHAR(50) NOT NULL);";
+
+        String criarTabelaUsuario = "CREATE TABLE IF NOT EXISTS " + TABELA_USUARIO
+                + "(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
+                + "username VARCHAR(20) NOT NULL,"
+                + "password VARCHAR(20) NOT NULL);";
+        try{
+            db.execSQL(criarTabelaTodo);
+            Log.i("INFO DB","Sucesso ao criar ao tabela Todo!");
+            db.execSQL(criarTabelaUsuario);
+            Log.i("INFO DB","Sucesso ao criar ao tabela Usuário!");
+        }catch(Exception e){
+            Log.i("INFO DB","Erro ao criar tabela "+e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        String sql = "DROP TABLE IF EXISTS " + TABELA_TODO +";";
+
+        try{
+            db.execSQL(sql);
+            onCreate(db);
+            Log.i("INFO DB","Sucesso ao criar ao tabela!");
+        }catch(Exception e){
+            Log.i("INFO DB","Erro ao criar tabela "+e.getMessage());
+        }
+
+
+    }
+}
